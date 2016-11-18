1640030c256aae0d85cbe68c44d8ee1255e0f3c8
rex.chen <rex.chen@104.com.tw>
2016-11-18 13:50:07 +0800

    map to for

--- src/index.js
+++ src/index.js
@@ -10,65 +10,71 @@ exports.default = function (babel) {
 
         if (target) {
           var parentName = target.id.name;
-
-          target.body.body.map(function (node) {
-            if (node.leadingComments && node.leadingComments[0].type === 'CommentBlock') {
-              var left = parentName;
-              var isDefined = false;
-              var comment = node.leadingComments[0].value.replace(/\*|\n|\t/g, "");
+          
+          if(target.body && target.body.body && target.body.body.length > 0){
+            for(var i = 0; i < target.body.body.length; i++){
+              var node = target.body.body[i];
               
-              if(comment){
-                var commentList = comment.split("@");
+              if (node.leadingComments && node.leadingComments[0].type === 'CommentBlock') {
+                var left = parentName;
+                var isDefined = false;
+                var comment = node.leadingComments[0].value.replace(/\*|\n|\t/g, "");
                 
-                commentList.map(function loop(expression, index) {
-                  expression = expression.trim();
+                if(comment){
+                  var commentList = comment.split("@");
                   
-                  if (index === 0) {
-                    if(/\s+/g.test(expression)){
-                      return;
-                    }
+                  for(var index = 0; index < commentList.length; index++){
+                    var expression = commentList[index];
+                    expression = expression.trim();
                     
-                    if(!expression){
-                      return;
-                    }
-                    
-                    if (expression !== 'constructor') {
-                      left += "." + expression;
-
-                      if (!isDefined) {
-                        isDefined = true;
-
-                        var defLeft = left;
-                        var defRight = "{}";
-                        var defLeftExpression = t.identifier(defLeft);
-                        var defRightExpression = t.identifier(defRight);
-                        var defAssignmentExpression = t.AssignmentExpression("=", defLeftExpression, defRightExpression);
-                        var defExpressionStatement = t.ExpressionStatement(defAssignmentExpression);
-
-                        path.node.body.splice(-1, 0, defExpressionStatement);
+                    if (index === 0) {
+                      if(/\s+/g.test(expression)){
+                        return;
                       }
-                    }
-                  } else {
-                    expression = expression.replace(/\s/g, "");
-                    
-                    var expressionList = expression.split(":");
-                    
-                    if(!expressionList[1]){
-                      return;
-                    }
-                    
-                    var newLeft = left + "['" + expressionList[0] + "']";
-                    var newRight = /^\d+|true|false$/gi.test(expressionList[1]) ? expressionList[1] : '"' + expressionList[1] + '"';
-                    var newLeftExpression = t.identifier(newLeft);
-                    var newRightExpression = t.identifier(newRight);
-                    var assignmentExpression = t.AssignmentExpression("=", newLeftExpression, newRightExpression);
-                    var expressionStatement = t.ExpressionStatement(assignmentExpression);
+                      
+                      if(!expression){
+                        return;
+                      }
+                      
+                      if (expression !== 'constructor') {
+                        left += "." + expression;
 
-                    path.node.body.splice(-1, 0, expressionStatement);
+                        if (!isDefined) {
+                          isDefined = true;
+
+                          var defLeft = left;
+                          var defRight = "{}";
+                          var defLeftExpression = t.identifier(defLeft);
+                          var defRightExpression = t.identifier(defRight);
+                          var defAssignmentExpression = t.AssignmentExpression("=", defLeftExpression, defRightExpression);
+                          var defExpressionStatement = t.ExpressionStatement(defAssignmentExpression);
+
+                          path.node.body.splice(-1, 0, defExpressionStatement);
+                        }
+                      }
+                    } else {
+                      expression = expression.replace(/\s/g, "");
+                      
+                      var expressionList = expression.split(":");
+                      
+                      if(!expressionList[1]){
+                        return;
+                      }
+                      
+                      var newLeft = left + "['" + expressionList[0] + "']";
+                      var newRight = /^\d+|true|false$/gi.test(expressionList[1]) ? expressionList[1] : '"' + expressionList[1] + '"';
+                      var newLeftExpression = t.identifier(newLeft);
+                      var newRightExpression = t.identifier(newRight);
+                      var assignmentExpression = t.AssignmentExpression("=", newLeftExpression, newRightExpression);
+                      var expressionStatement = t.ExpressionStatement(assignmentExpression);
+
+                      path.node.body.splice(-1, 0, expressionStatement);
+                    }
                   }
-                });
-              }            }
-          });
+                }
+              }
+            }
+          }
         }
       }
     }
